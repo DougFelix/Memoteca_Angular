8591997a0f848f33d80ac3395cbafd94ebe30f2a
DougFelix <dotfreitas@gmail.com>
2023-09-10 16:23:43 -0300

    Angular Study - Diretivas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-
+  listaPensamentos = [
+    {
+      conteudo: 'I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular, I love Angular,I love Angular, I love Angular, I love Angular,',
+      autoria: 'Douglas',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Hate is a strong word',
+      autoria: 'Douglas',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lets be friends',
+      autoria: 'Douglas',
+      modelo: 'modelo1'
+    }
+  ];
 }
