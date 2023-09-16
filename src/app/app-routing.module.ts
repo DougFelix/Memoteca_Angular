3b2eb34b387a089eb34177f8310ac23539e71248
DougFelix <dotfreitas@gmail.com>
2023-09-16 17:04:20 -0300

    Angular Study - HttpClient, Observable e CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'listarPensamento'
+    redirectTo: 'listarPensamento',
   },
   {
     path: 'criarPensamento',
@@ -15,7 +17,15 @@ const routes: Routes = [
   },
   {
     path: 'listarPensamento',
-    component: ListarPensamentoComponent
+    component: ListarPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/editarPensamento/:id',
+    component: EditarPensamentoComponent,
   }
 ];
 
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,23 +1,29 @@
+import { PensamentoService } from './../pensamento.service';
 import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent {
-  pensamento  : Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: 'modelo1'
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  constructor(private service: PensamentoService, private router: Router) {}
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
   }
 
-  criarPensamento(){
-    alert("Novo pensamento criado");
-  }
-
-  cancelar(){
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
   }
 }
